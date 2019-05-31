46b24483b4fd613531aaa26e9a13641546a75d01
Prasad, Ramanuj (ITCDEC) - KLM <Ramanuj.Prasad@klm.com>
2019-05-31 10:25:18 +0200

    updated the project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to my-profile!');
+    expect(page.getTitleText()).toEqual('Welcome to portfolio!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-profile'`, () => {
+  it(`should have as title 'portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-profile');
+    expect(app.title).toEqual('portfolio');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-profile!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to portfolio!');
   });
 });
